1fd93c23028648086da06e3102ea518def17841b
roberto <roberto.judet@itschool.ro>
2021-12-13 21:00:13 +0200

    Add homework 8

---
+++ homework/homework8.py
@@ -0,0 +1,9 @@
+list1 = ["Michael", "", "Ellie", "Ken", "", "Gunter"]
+# write a function which returns a list without the empty strings (do not use remove function for this exercise)
+
+list2 = [1, -2, 3, -4, 10, 8, 99, -23, 55, 100, -0.6, 27.6]
+# write a function which returns a dict containg the 2nd largest number & 2nd smallest number
+# write a function which makes the sum of all positive numbers and multiplies all the negative ones
+
+list3 = ["a", "a", "c", "d", "b", "c", "a"]
+# write a function which counts every element and returns a dict {"a": 3, "b": ...
