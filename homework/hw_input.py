400fd9f9e2a0e79eb1bfccb4f33d47517672e331
roberto <roberto.judet@itschool.ro>
2021-12-17 11:06:10 +0200

    Homework with input exercises

---
+++ homework/hw_input.py
@@ -0,0 +1,21 @@
+#ex1
+# take 2 numbers from the user
+# print the sum back
+
+#ex2
+# take 3 numbers from the user
+# print the biggest one
+
+#ex3
+#ask the user's name
+#ask him his age
+# print if he is allowed to drink
+
+#ex4
+#ask the user for at least 5 words
+#put them in a sentence
+
+#ex5
+import random
+x = random.randint(0, 9) #this will pick a random number between 0 and 9
+#create a guessing game, the user must tell a number between 0 and 9 and you must tell him if he guessed it
