f94eaf7ff037182589a0840fd1defbe75a785497
roberto <roberto.judet@itschool.ro>
2021-12-25 12:52:27 +0200

    Homework part1 for holidays

---
+++ homework/christmas_present.py
@@ -0,0 +1,29 @@
+dict1 = {"a": 1, "b": 3, "c": 6, "d": 10, "e": 90, "f": 15}
+# ex1 - define a function which returns the keys of a dictionary as a list
+# ex2 - define a function which returns the biggest number from a dictionary
+# ex3 - define a function which removes all odd numbers from a dictionary
+
+tuple1 = ("a", "b", "c", "d")
+tuple2 = (1, 2, 3, 4)
+# ex4 - define a function which receives 2 tuples and creates a dictionary (one tuple will become the keys)
+
+# ex5 - define a function which makes the sum of the values from a dictionary
+# ex6 - define a function which creates a string from the keys of a dictionary
+
+prices = {"iphone": 1000, "samsung": 800, "pixel": 600, "allview": 100, "nokia": 200, "oppo": 600}
+# ex7 - define a function which receives a dictionary (keys are the phone, values are the price)
+# and the budget (min & max) and return all the phones which are in the price interval
+
+accounts = [
+    {"user": "Bob", "account": 0, "debt": 500},
+    {"user": "Alice", "account": 2000, "debt": 0},
+    {"user": "Leo", "account": 600, "debt": 2000},
+    {"user": "Ralph", "account": 5000, "debt": 1000},
+    {"user": "Matilda", "account": 100, "debt": 0}
+]
+# ex8 - define a function which receives a list like the one above and returns the user with the biggest account and
+# the use with the biggest difference between debt and account
+
+# ex9 - ask the user to give a name, email and telephone and write to a dict, print the dict
+
+#ex10 - create a rock, paper, scissors game with the user's input
