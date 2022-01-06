7c33340e013efde8cd0efe218c10d95f0ba35aa5
roberto <roberto.judet@itschool.ro>
2022-01-06 14:34:47 +0200

    Solve part of the holiday homeworks & introduce GET API requsets

--- homework/christmas_present.py
+++ homework/christmas_present.py
@@ -1,14 +1,57 @@
 dict1 = {"a": 1, "b": 3, "c": 6, "d": 10, "e": 90, "f": 15}
+
+
 # ex1 - define a function which returns the keys of a dictionary as a list
+#     dict1.keys()
 # ex2 - define a function which returns the biggest number from a dictionary
+#     max(dict1.values())
+def max_of_dict(d: dict):
+    return max(d.values())
+
+
+max = max_of_dict(dict1)
+print(max)
+
+
 # ex3 - define a function which removes all odd numbers from a dictionary
+def remove_odd_numbers(d: dict):  # we create a new list w/o the odd numbers
+    return {k: v for k, v in d.items() if v % 2 == 0}
+
+
+dict1 = remove_odd_numbers(dict1)
+print(dict1)
+#   dict1.pop(k)  remove the key, but not in for
+
 
 tuple1 = ("a", "b", "c", "d")
 tuple2 = (1, 2, 3, 4)
+
+
 # ex4 - define a function which receives 2 tuples and creates a dictionary (one tuple will become the keys)
+def create_dict_from_tuples(tuple1: tuple, tuple2: tuple) -> dict:
+    new_dict = {}
+    i = 0
+    while i < len(tuple1) or i < len(tuple2):
+        key = tuple1[i]
+        value = tuple2[i]
+        new_dict[key] = value
+        i = i + 1
+    return new_dict
+
+
+dict4 = create_dict_from_tuples(tuple1, tuple2)
+
 
 # ex5 - define a function which makes the sum of the values from a dictionary
+#     sum(dict1.values())
 # ex6 - define a function which creates a string from the keys of a dictionary
+def create_string_from_keys(dict1: dict) -> str:
+    keys = dict1.keys()  # collect the keys
+    return "-".join(keys)  # glue/join them in a string
+
+
+str2 = create_string_from_keys(dict4)
+print(str2)
 
 prices = {"iphone": 1000, "samsung": 800, "pixel": 600, "allview": 100, "nokia": 200, "oppo": 600}
 # ex7 - define a function which receives a dictionary (keys are the phone, values are the price)
@@ -26,4 +69,4 @@ accounts = [
 
 # ex9 - ask the user to give a name, email and telephone and write to a dict, print the dict
 
-#ex10 - create a rock, paper, scissors game with the user's input
+# ex10 - create a rock, paper, scissors game with the user's input
