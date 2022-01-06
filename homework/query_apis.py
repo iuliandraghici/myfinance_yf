8b4061d92591df6525fcba1d224b81754a4b364b
roberto <roberto.judet@itschool.ro>
2022-01-06 14:51:45 +0200

    Add API GET homework

---
+++ homework/query_apis.py
@@ -0,0 +1,17 @@
+#ex1
+# create a function which receives a desired number of animals & returns a list of their names
+# you can use the URL https://zoo-animal-api.herokuapp.com/animals/rand/{number_of_animals}
+
+#ex2
+# we can get the list of cryptocurrency coins from here https://api.coingecko.com/api/v3/coins/list
+# create a method which lists all the names
+# create a method which returns the symbol, name for a given id
+
+#ex3
+# let's query the NASA picture of the day
+# url https://api.nasa.gov/planetary/apod?
+# you will need an api key, you can use the demo key as a query param
+# requests.get("https://api.nasa.gov/planetary/apod", params={"api_key": "DEMO_KEY"})
+# in the dict of params you can also add other params, you can check them in nasa_query_params.png
+# create a function which returns the explanation & the link of the image for a given date
+
