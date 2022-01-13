774f5cb7a5b10490dbdf4c16b1b6c94d0588e041
roberto <roberto.judet@itschool.ro>
2022-01-13 20:20:56 +0200

    Add homework for creating a web server

---
+++ homework/web_server.py
@@ -0,0 +1,8 @@
+# create a web server to keep a list of books
+# when we call POST we can add a book to the list, we specify the name & how many pages it has
+# when we call DELETE we just specify the name & remove the book from a list
+# we can record our reading progress, how many pages we read, we use PUT and specify the name of the book and
+# how many pages we read since our last input (we need to keep a value of pages_read and add to it the new number)
+# GET returns a list of the book names & the percentage of how much we read
+
+
