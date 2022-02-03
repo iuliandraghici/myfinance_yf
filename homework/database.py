7b765a7a86b853ecdc1155cc7c9b3d8f28d73d10
roberto <roberto.judet@itschool.ro>
2022-02-03 13:22:24 +0200

    Add homework database

---
+++ homework/database.py
@@ -0,0 +1,13 @@
+# create a database for a hospital, make these tables: patients, doctors & medicine
+# patients have a name, email, telephone & a family doctor assigned
+# doctors have a name & specialty
+# medicine has a name, type & number of items in stock
+# create method for removing an amount of medicine from stock
+# create methods for adding a medicine, patient & doctors
+# create method for removing patient & doctors
+# create method for listing all medicines & doctors
+
+
+# create an app for having a shopping list, we will save it as JSON in a file
+# should have an API, we can add a new item (name & number), we can delete it, we can edit the number of items,
+# we can see the whole list
